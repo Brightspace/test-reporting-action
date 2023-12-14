9ffdcb27c6d831aa4a465d5f1ffac884129cb5e6
Devon Powell <Devon.Powell@D2L.com>
2023-12-14 10:11:57 -0500

    Add report loading and validating (#3)

---
+++ src/report.js
@@ -0,0 +1,131 @@
+import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
+import fs from 'fs/promises';
+
+const ajv = new Ajv({ verbose: true });
+
+addFormats(ajv, ['date-time', 'uri', 'uuid' ]);
+
+const schema = {
+	type: 'object',
+	properties: {
+		reportId: { type: 'string', format: 'uuid' },
+		reportVersion: { type: 'integer', const: 1 },
+		summary: {
+			type: 'object',
+			properties: {
+				githubOrganization: { type: 'string', pattern: '[A-Za-z0-9_.-]+' },
+				githubRepository: { type: 'string', pattern: '[A-Za-z0-9_.-]+' },
+				githubWorkflow: { type: 'string', pattern: '^[^\\s].+[^\\s]$' },
+				githubRunId: { type: 'integer', minimum: 0 },
+				githubRunAttempt: { type: 'integer', minimum: 1 },
+				gitBranch: { type: 'string', pattern: '^[^\\s].+[^\\s]$' },
+				gitSha: { type: 'string', pattern: '([A-Fa-f0-9]{40})' },
+				lmsBuild: { type: 'string', pattern: '([0-9]{2}\\.){3}[0-9]{5}' },
+				lmsInstance: { type: 'string', format: 'uri' },
+				operatingSystem: { type: 'string', enum: ['windows', 'linux', 'mac'] },
+				framework: { type: 'string', pattern: '^[^\\s].+[^\\s]$' },
+				started: { type: 'string', format: 'date-time' },
+				totalDuration: { type: 'integer', minimum: 0 },
+				status: { type: 'string', enum: ['passed', 'failed'] },
+				countPassed: { type: 'integer', minimum: 0 },
+				countFailed: { type: 'integer', minimum: 0 },
+				countSkipped: { type: 'integer', minimum: 0 },
+				countFlaky: { type: 'integer', minimum: 0 }
+			},
+			required: [
+				'githubOrganization',
+				'githubRepository',
+				'githubWorkflow',
+				'githubRunId',
+				'githubRunAttempt',
+				'gitBranch',
+				'gitSha',
+				'operatingSystem',
+				'framework',
+				'started',
+				'totalDuration',
+				'status',
+				'countPassed',
+				'countFailed',
+				'countSkipped',
+				'countFlaky'
+			],
+			additionalProperties: false
+		},
+		details: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					name: { type: 'string', pattern: '^[^\\s].+[^\\s]$' },
+					location: { type: 'string', pattern: '^[^\\s].+[^\\s]$' },
+					started: { type: 'string', format: 'date-time' },
+					duration: { type: 'integer', minimum: 0 },
+					totalDuration: { type: 'integer', minimum: 0 },
+					status: { type: 'string', enum: ['passed', 'failed', 'skipped'] },
+					tool: { type: 'string', pattern: '^[^\\s].+[^\\s]$' },
+					experience: { type: 'string', pattern: '^[^\\s].+[^\\s]$' },
+					type: { type: 'string', pattern: '^[^\\s].+[^\\s]$' },
+					browser: { type: 'string', enum: ['chromium', 'firefox ', 'webkit'] },
+					retries: { type: 'integer', minimum: 0 }
+				},
+				required: [
+					'name',
+					'location',
+					'started',
+					'duration',
+					'totalDuration',
+					'status',
+					'retries'
+				],
+				additionalProperties: false
+			}
+		}
+	},
+	required: [
+		'reportId',
+		'reportVersion',
+		'summary',
+		'details'
+	],
+	additionalProperties: false
+};
+
+const validate = ajv.compile(schema);
+
+const getReport = async(logger, context, inputs) => {
+	logger.startGroup('Gathering test report');
+
+	const { reportPath } = inputs;
+	let report;
+
+	try {
+		const reportRaw = await fs.readFile(reportPath, 'utf8');
+
+		report = JSON.parse(reportRaw);
+	} catch {
+		throw new Error('Report is not valid');
+	}
+
+	logger.info('Injecting GitHub context');
+
+	report.summary = {
+		...context,
+		...report.summary
+	};
+
+	logger.info('Validating test report');
+
+	if (!validate(report)) {
+		const message = ajv.errorsText(validate.errors, { dataVar: 'report' });
+
+		throw new Error(`Report does not conform to needed schema: ${message}`);
+	}
+
+	logger.endGroup();
+
+	return report;
+};
+
+export { getReport };
---
+++ test/report.test.js
@@ -0,0 +1,204 @@
+import { expect } from 'chai';
+import fs from 'fs/promises';
+import { getReport } from '../src/report.js';
+import { createSandbox } from 'sinon';
+
+const context = {
+	githubOrganization: 'TestOrganization',
+	githubRepository: 'test-repository',
+	githubWorkflow: 'test-workflow.yml',
+	githubRunId: 12345,
+	githubRunAttempt: 1,
+	gitBranch: 'test/branch',
+	gitSha: '0000000000000000000000000000000000000000'
+};
+const inputs = {};
+const reportPartial = {
+	reportId: '00000000-0000-0000-0000-000000000000',
+	reportVersion: 1,
+	summary: {
+		operatingSystem: 'linux',
+		framework: 'mocha',
+		started: (new Date()).toISOString(),
+		totalDuration: 23857,
+		status: 'passed',
+		countPassed: 2,
+		countFailed: 0,
+		countSkipped: 1,
+		countFlaky: 1
+	},
+	details: [
+		{
+			name: 'test suite > flaky test',
+			location: 'test/test-suite.js',
+			started: (new Date()).toISOString(),
+			duration: 237,
+			totalDuration: 549,
+			status: 'passed',
+			retries: 1
+		},
+		{
+			name: 'test suite > passing test',
+			location: 'test/test-suite.js',
+			started: (new Date()).toISOString(),
+			duration: 237,
+			totalDuration: 237,
+			status: 'passed',
+			retries: 0
+		},
+		{
+			name: 'test suite > skipped test',
+			location: 'test/test-suite.js',
+			started: (new Date()).toISOString(),
+			duration: 0,
+			totalDuration: 0,
+			status: 'skipped',
+			retries: 0
+		}
+	]
+};
+const reportFull = {
+	reportId: '00000000-0000-0000-0000-000000000000',
+	reportVersion: 1,
+	summary: {
+		lmsBuild: '20.24.01.12345',
+		lmsInstance: 'https://cd2024112345.devlms.desire2learn.com/',
+		operatingSystem: 'linux',
+		framework: 'mocha',
+		started: (new Date()).toISOString(),
+		totalDuration: 23857,
+		status: 'passed',
+		countPassed: 2,
+		countFailed: 0,
+		countSkipped: 1,
+		countFlaky: 1
+	},
+	details: [
+		{
+			name: 'test suite > flaky test',
+			location: 'test/test-suite.js',
+			started: (new Date()).toISOString(),
+			duration: 237,
+			totalDuration: 549,
+			status: 'passed',
+			retries: 1
+		},
+		{
+			name: 'test suite > passing test',
+			location: 'test/test-suite.js',
+			started: (new Date()).toISOString(),
+			duration: 237,
+			totalDuration: 237,
+			status: 'passed',
+			retries: 0
+		},
+		{
+			name: 'test suite > skipped test',
+			location: 'test/test-suite.js',
+			started: (new Date()).toISOString(),
+			duration: 0,
+			totalDuration: 0,
+			status: 'skipped',
+			retries: 0
+		}
+	]
+};
+
+describe('report', () => {
+	let sandbox;
+
+	before(() => sandbox = createSandbox());
+
+	afterEach(() => sandbox.restore());
+
+	const makeDummyLogger = () => ({
+		startGroup: sandbox.stub(),
+		endGroup: sandbox.stub(),
+		info: sandbox.stub(),
+		error: sandbox.stub()
+	});
+
+	describe('get report', () => {
+		let logger;
+
+		beforeEach(() => logger = makeDummyLogger());
+
+		it('partial', async() => {
+			sandbox.stub(fs, 'readFile').resolves(JSON.stringify(reportPartial));
+
+			const report = await getReport(logger, context, inputs);
+			const { reportId, reportVersion, summary } = report;
+
+			expect(reportId).to.eq(reportPartial.reportId);
+			expect(reportVersion).to.eq(reportPartial.reportVersion);
+			expect(summary.githubOrganization).to.eq(context.githubOrganization);
+			expect(summary.githubRepository).to.eq(context.githubRepository);
+			expect(summary.githubWorkflow).to.eq(context.githubWorkflow);
+			expect(summary.githubRunId).to.eq(context.githubRunId);
+			expect(summary.githubRunAttempt).to.eq(context.githubRunAttempt);
+			expect(summary.gitBranch).to.eq(context.gitBranch);
+			expect(summary.gitSha).to.eq(context.gitSha);
+		});
+
+		it('full', async() => {
+			sandbox.stub(fs, 'readFile').resolves(JSON.stringify(reportFull));
+
+			const report = await getReport(logger, context, inputs);
+			const { reportId, reportVersion, summary } = report;
+
+			expect(reportId).to.eq(reportFull.reportId);
+			expect(reportVersion).to.eq(reportFull.reportVersion);
+			expect(summary.githubOrganization).to.eq(context.githubOrganization);
+			expect(summary.githubRepository).to.eq(context.githubRepository);
+			expect(summary.githubWorkflow).to.eq(context.githubWorkflow);
+			expect(summary.githubRunId).to.eq(context.githubRunId);
+			expect(summary.githubRunAttempt).to.eq(context.githubRunAttempt);
+			expect(summary.gitBranch).to.eq(context.gitBranch);
+			expect(summary.gitSha).to.eq(context.gitSha);
+		});
+
+		describe('fails', () => {
+			it('file read', async() => {
+				sandbox.stub(fs, 'readFile').throws();
+
+				try {
+					await getReport(logger, context, inputs);
+				} catch (err) {
+					expect(err.message).to.contain('Report is not valid');
+
+					return;
+				}
+
+				throw new Error('failed');
+			});
+
+			it('not json', async() => {
+				sandbox.stub(fs, 'readFile').resolves('this is not json');
+
+				try {
+					await getReport(logger, context, inputs);
+				} catch (err) {
+					expect(err.message).to.contain('Report is not valid');
+
+					return;
+				}
+
+				throw new Error('failed');
+			});
+
+			it('validation', async() => {
+				sandbox.stub(fs, 'readFile').resolves('{}');
+
+				try {
+					await getReport(logger, context, inputs);
+				} catch (err) {
+					expect(err.message).to.contain('Report does not conform to needed schema');
+
+					return;
+				}
+
+				throw new Error('failed');
+			});
+		});
+	});
+});
