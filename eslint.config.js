d7bf2d33c685cf00d61374929dbd3b7f313ca0f9
Devon Powell <Devon.Powell@D2L.com>
2024-10-30 09:35:40 -0400

    Update eslint config and remove legacy mode set-up (#518)

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import { includeIgnoreFile } from '@eslint/compat';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+});
+
+export default [
+	includeIgnoreFile(gitignorePath),
+	{ ignores: ['dist/'] },
+	...compat.extends('brightspace/node-config').map((config) => ({
+		...config,
+		files: ['**/*.js', '**/*.cjs']
+	})),
+	...compat.extends('brightspace/testing-config').map((config) => ({
+		...config,
+		files: ['test/**/*.test.js']
+	}))
+];
