d2e2793bd3b03472c4f53b8c5e23873bb43d8c03
Devon Powell <devon.f.powell@gmail.com>
2024-11-27 15:17:32 -0500

    More eslint adjustments (#567)

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,44 @@
 import { dirname, resolve } from 'node:path';
-import { nodeConfig, setDirectoryConfigs, testingConfig } from 'eslint-config-brightspace';
+import { nodeConfig, setDirectoryConfigs } from 'eslint-config-brightspace';
 import { fileURLToPath } from 'node:url';
-import globals from 'globals';
 import { includeIgnoreFile } from '@eslint/compat';
 import jsonPlugin from 'eslint-plugin-json';
+import mochaPlugin from 'eslint-plugin-mocha';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const jsonConfig = jsonPlugin.configs['recommended'];
 const gitignorePath = resolve(__dirname, '.gitignore');
+const ignoreConfigs = [
+	includeIgnoreFile(gitignorePath),
+	{ ignores: ['**/dist/'] }
+];
+const globalConfigs = [
+	...nodeConfig,
+	{
+		rules: {
+			'comma-dangle': 'error'
+		}
+	}
+];
+const testConfigs = [
+	...globalConfigs,
+	mochaPlugin.configs.flat.recommended,
+	{
+		rules: {
+			'mocha/no-exclusive-tests': 'error',
+			'mocha/no-mocha-arrows': 'off'
+		}
+	}
+];
+const jsonConfig = jsonPlugin.configs['recommended'];
 
 export default [
-	includeIgnoreFile(gitignorePath),
-	{ ignores: ['**/dist/'] },
+	...ignoreConfigs,
 	...setDirectoryConfigs(
-		nodeConfig,
+		globalConfigs,
 		{
-			'test/': testingConfig
+			'test/': testConfigs
 		}
 	),
-	{
-		files: ['test/**/*.js'],
-		languageOptions: {
-			globals: {
-				...globals.node
-			}
-		}
-	},
 	jsonConfig
 ];
