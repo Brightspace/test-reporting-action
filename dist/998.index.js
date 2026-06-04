4503bf67198ac640c85ba2aefe38dcbffb56d717
devpow112 <9043211+devpow112@users.noreply.github.com>
2026-06-04 01:38:30 +0000

    Update Build

--- dist/998.index.js
+++ dist/998.index.js
@@ -297,7 +297,7 @@ const commonParams = {
     UseDualStack: { type: "builtInParams", name: "useDualstackEndpoint" },
 };
 
-var version = "3.997.12";
+var version = "3.997.14";
 var packageInfo = {
 	version: version};
 
